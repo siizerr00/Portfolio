c2dea89913ec2f1650ad3847edd9fbd6bdf7dec7
Aulia Tataning N <127916387+siizerr00@users.noreply.github.com>
2025-12-11 11:37:32 +0700

    Update new_user_purchase.sql

--- [Kulina] Data Analyst - Preliminary Test/Database and SQL/new_user_purchase.sql
+++ [Kulina] Data Analyst - Preliminary Test/Database and SQL/new_user_purchase.sql
@@ -10,5 +10,5 @@ join ku_product kp on kod.product_id = kp.id
 join ku_order ko on ko.id = kod.order_id
 join ku_user ku on ku.id = ko.user_id
 where kod.status = 2 and
-kod.delivery_date between date(ku.created_at) and DATE_ADD(DATE(ku.created_at), INTERVAL 1 YEAR)
+kod.delivery_date between date(ku.created_at) and DATE_ADD(DATE(ku.created_at), INTERVAL 7 DAY)
 group by 1,2,3,4,5,6
